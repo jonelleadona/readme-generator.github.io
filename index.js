3171553eeb9a818067f4fd35805f96121b603fa9
Jonelle Adona <jonelle.adona@knights.ucf.edu>
2020-11-10 22:50:55 -0500

    Add question array

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // array of questions for user
 const questions = [
-
+"Please enter title", 
+"Please enter description",
+"Please enter installation instructions", 
+"Please enter usage information", 
+"Please enter contribution guidelines", 
+"Please enter test instructions",
+"Please enter username",
+"Please enter email adress",
+"Please enter license",
 ];
+ console.log(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {
+  
 }
 
 // function to initialize program
